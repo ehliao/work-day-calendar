eb29375bf70193970393c0d6fe068050930feaaf
unknown <you@example.com>
2022-05-30 21:34:20 -0700

    updated js, removed console logs

--- script.js
+++ script.js
@@ -11,13 +11,15 @@ var counter = 1
 //build the calendar div for 9am to 5pm
 var timeblock = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM','4PM', '5PM'];
 for (let i = 0; i < timeblock.length; i++) {
+    var timeID = timeblock[i]+" block"
     $(".container").append(timeRow);
     var time = $("<div>").addClass("col-2 colorcode").text(timeblock[i]);
     $(".timeblock").append(time);
-    var editContent = $("<input>").addClass("col-9 edit-cont"+counter);
+    var editContent = $("<input>").addClass("col-9 edit-cont"+counter).attr('id', timeID);
     $(".timeblock").append(editContent);
     var saveButton = $("<button>").addClass("col-1 saveBtn").text("Save");
     $(".timeblock").append(saveButton);    
+
 
     // var and statment to assign color based on past, present, future
     var newTime = parseInt(timeblock[i].slice(0,timeblock[i].length -2));
@@ -32,14 +34,27 @@ for (let i = 0; i < timeblock.length; i++) {
     } else {
         $(".edit-cont"+counter).addClass("past");
     }
-    console.log("HERRO", newTime)
-    console.log(currentHour)
     counter ++;
 };
 
-// save input from save button
-localStorage.setItem();
-localStorage.getItem();
+$("button").click(function () {
+    $("input").each(function() {    
+       var id = $(this).attr('id');
+       var value = $(this).val();
+      localStorage.setItem(id, value);
+   }); 
+});
+
+$(document).ready(function() {
+    $('input').each(function(){    
+        var id = $(this).attr('id');
+        var value = localStorage.getItem(id);
+        
+        $(this).val(value);
+        
+    }); 
+});
+
 
 
 
